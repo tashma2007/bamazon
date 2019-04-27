4caac3535e078ac97fbd2942d97be2ac11d1e032
Natasha Henry <natashaphenry@yahoo.com>
2019-04-27 01:02:13 -0400

    adding js code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,16 +7,90 @@ var conn = mysql.createConnection(connString);
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-host: "localhost",
+    host: "localhost",
 
-port: 8889,
-user: "root",
-password: "root",
-database: "bamazonDB"
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "bamazonDB"
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
-if (err) throw err;
-console.log("connection successful!");
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connection successful!");
+    queryAllProducts();
+    start();
 });
+
+function queryAllProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+        }
+    });
+}
+function start() {
+    inquirer
+        .prompt([
+            {
+                name: "productId",
+                type: "input",
+                message: "What is the ID number of the product that you would like to buy?",
+                validate: function (value) {
+                    if (isNaN(value) == false) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many units of the product would you like to buy?",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+
+            }
+        ])
+        .then(function (answer) {
+            connection.query(`SELECT * FROM products WHERE productId = ${answer.productId}`, function (err, res) {
+                if (err) throw err;
+
+                if (parseInt(res[0].stock_quantity) >= parseInt(answer.quantity)) {
+                    totalCost = answer.quantity * res[0].price;
+                    sellInventory(res, answer);
+                }
+                else {
+                    console.log(`
+Insufficient Quantity. We only have ${res[0].stock_quantity} ${res[0].product_name} in stock.`);
+                    userPrompt();
+                }
+            })
+        })
+}
+
+// 
+function sellInventory(res, answer) {
+    console.log(`
+Congratulations. You've purchased ${answer.quantity} ${res[0].product_name}(s). Your total is $${totalCost}`)
+    connection.query(`UPDATE products SET ? WHERE ?`,
+        [
+            {
+                stock_quantity: res[0].stock_quantity - answer.quantity,
+            },
+            {
+                id: answer.id
+            }
+        ]);
+    endConnection();
+};
+
+function endConnection() {
+    connection.end();
+}
---
+++ bamazonDB.sql
@@ -0,0 +1,49 @@
+DROP DATABASE IF EXISTS bamazonDB;
+
+CREATE DATABASE bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products (
+id INTEGER(11) AUTO_INCREMENT NOT NULL,
+product_name VARCHAR(100) NULL,
+department_name VARCHAR(100) NULL,
+price DECIMAL(10,4) NULL,
+stock_quantity DECIMAL(10,4) NULL,
+PRIMARY KEY (id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("cell phone case", "cell phone accessories", 7.50, 15);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("2013 Mac Book Pro", "electronics", 567.50, 1);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Hyao Mirazaki's Ultimate Collection", "movies", 21.50, 5);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Amythest Crystal Necklace", "jewelry", 17.50, 2);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Amythest Crystal Necklace", "jewelry", 17.50, 2);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("14K White Gold Hoop Earrings", "jewelry", 97.50, 5);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("2019 Kindle Fire HD 32GB", "electronics", 147.50, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Cosyzone Car Windshield Sunshade Four Seasons", "automotive", 10.49, 5);
+
+NSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Copper Chef Electric Egg Cooker Set 7 or 14", "Home & Kitchen", 17.50, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Cocori Air Fryer, 5.8 QT", "Home $ Kithcne", 117.50, 1);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Top Plaza Antique Silver Flowers Wrapped Natural Rose Quartz", "Jewlry", 8.99, 10);
+
+SELECT * FROM products;
